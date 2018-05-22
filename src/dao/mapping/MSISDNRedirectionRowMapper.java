0bf6a24eee73b257502456ff662d5318b22f04e3
Sedar <Sedar@MOHRADES.mshome.net>
2018-05-22 11:01:27 +0100

    2018-05-22 11:01 AM Commit

--- src/dao/mapping/MSISDNRedirectionRowMapper.java
+++ src/dao/mapping/MSISDNRedirectionRowMapper.java
@@ -6,13 +6,13 @@ import org.springframework.jdbc.core.RowMapper;
 
 import domain.models.MSISDNRedirection;
 
-public class MSISDNRedirectionRowMapper implements RowMapper<MSISDNRedirection>{
+public class MSISDNRedirectionRowMapper implements RowMapper<MSISDNRedirection> {
 
 	@Override
 	public MSISDNRedirection mapRow(ResultSet rs, int rowNum) throws SQLException {
 		// TODO Auto-generated method stub
 
-		return new MSISDNRedirection(rs.getInt("ID"), rs.getInt("SERVICE_CODE"), rs.getString("TYPE"), rs.getString("EXPRESSION"));
+		return new MSISDNRedirection(rs.getInt("ID"), rs.getInt("SERVICE_CODE"), rs.getString("TYPE"), rs.getString("EXPRESSION"), rs.getString("REDIRECTION_URL"));
 	}
 
 }
